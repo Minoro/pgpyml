8bc4961a63d3f0d91a858e7847a0f82e1000a952
Minoro Fusioka <minorofusioka@hotmail.com>
2021-04-14 22:59:49 -0300

    Basic functions with python

---
+++ src/scripts/example_python_functions.sql
@@ -0,0 +1,19 @@
+-- CREATE EXTENSION plpython3u;
+
+-- Basic function with python
+CREATE OR REPLACE FUNCTION say_hello_world() RETURNS TEXT AS
+$$
+  return 'Hello, World'
+$$ LANGUAGE plpython3u;
+
+-- Basic function with argument
+CREATE OR REPLACE FUNCTION greet_person(name text) RETURNS TEXT AS
+$$
+  greeting = 'Hello, ' + name
+  return greeting
+$$ LANGUAGE plpython3u;
+*/
+
+-- SELECT * FROM say_hello_world()
+
+-- SELECT * FROM greet_person('Maya');
